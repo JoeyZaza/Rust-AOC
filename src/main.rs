cb3b7edfc17ad78ddcd722a7a632300b85eb1f11
JoeyZaza <61400998+JoeyZaza@users.noreply.github.com>
2024-10-03 10:05:24 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let res = day3::main(file); //DEBUGGING DONE: Capital Z's were getting returned as Some(0) instead of Some(52)
                                 // cuz I'm an idiot and was looping through range 1..table.len() and forgetting that
-                                //the table's length was 52 and Rust's range is upper bounds exclusing (or whatever it's called)
+                                //the table's length was 52 and Rust's range is upper bounds exclusive (or whatever it's called)
                                 //Edit: took me way too long to debug this....am I dumb?
                                 //Edit: yeah I'm dumb
                                 //Edit: also I can't actually debug because GDB refuses to work for me..probably cuz I'm dumb
